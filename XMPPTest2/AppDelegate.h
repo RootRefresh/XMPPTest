ac2ec8f22c499697c13d7719f46ad4d3533e82aa
mac <xuming@yy.local>
2016-04-05 18:36:20 +0800

    Initial Commit

---
+++ XMPPTest2/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  XMPPTest2
+//
+//  Created by USER on 16/4/5.
+//  Copyright © 2016年 Refresh_Yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ XMPPTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPTest2
+//
+//  Created by USER on 16/4/5.
+//  Copyright © 2016年 Refresh_Yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
